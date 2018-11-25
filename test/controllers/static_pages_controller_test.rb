cdc170d04c457f9f666f4963d06eca6cc2d2e0da
Daniil Turaev <adef96@mail.ru>
2018-11-25 14:33:39 +0300

    Home page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,6 +9,20 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "help"
   end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "about"
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "home"
+  end
+
 
 end
