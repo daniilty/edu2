0e3ea4feb6006e3f8f7258a460ccd485b2518370
Daniil Turaev <adef96@mail.ru>
2018-11-25 15:10:28 +0300

    Modded home page

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,11 @@
 module StaticPagesHelper
+	#returns full title
+	def full_title (page_title = '')
+		base_title = "Sample app"
+		if page_title.blank?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
