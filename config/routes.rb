05ab45670bb06d1508d8f7651ae929fd32df74cf
Daniil Turaev <adef96@mail.ru>
2018-12-08 17:13:39 +0300

    Added a User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/sign'
+  get 'static_pages/signup'
   root "static_pages#home"# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
