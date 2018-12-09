d37082763b496d2f1a8cd2683144e871b566836a
Daniil Turaev <adef96@mail.ru>
2018-12-09 14:33:56 +0300

    Sign up(working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   def new
-  	@user = User.create
+  	@user = User.new
   end
   def show
   	@user = User.find(params[:id])
+  end
+  def create
+  	@user = User.new(params.require(:user).permit(:name,:email,:password,:password_confirmation))
+  	if @user.save
+  		flash[:success] = "Hello"
+  		redirect_to(@user)
+  	else
+  		render '/users/new'
+  	end	
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-	before_save { email.downcase! }
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, uniqueness: { case_sensitive: false }
 	validates :password, presence: true, length: { minimum: 6 }
