c1c64be6517fbe1d446140dcf5dfcf7f7f36efe5
Daniil Turaev <adef96@mail.ru>
2018-12-10 15:42:47 +0300

    Burger menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/sign'
   get 'static_pages/signup'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy' 
   resources :users
   root "static_pages#home"# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
